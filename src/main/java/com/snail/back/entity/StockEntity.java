e9e80f13f3bb9aed61f85e1132fc3ba3caf07446
Snail <snail1704@gmail.com>
2023-08-16 13:11:51 +0400

    Delete "to lowercase"

--- src/main/java/com/snail/back/entity/StockEntity.java
+++ src/main/java/com/snail/back/entity/StockEntity.java
@@ -10,19 +10,19 @@ import org.springframework.data.relational.core.mapping.Table;
 @NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "stocks")
+@Table(name = "STOCKS")
 public class StockEntity {
 
     @Id
-    @Column(value = "id")
+    @Column(value = "ID")
     private Long id;
 
-    @Column(value = "date")
+    @Column(value = "DATE")
     private Long date;
 
-    @Column(value = "name")
+    @Column(value = "NAME")
     private String name;
 
-    @Column(value = "cost")
+    @Column(value = "COST")
     private Integer cost;
 }
